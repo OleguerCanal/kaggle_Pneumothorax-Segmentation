999ac59fd444e89bc43de073afbed9076e6bdc34
oleguer <oleguer.canal@hotmail.com>
2019-07-09 15:09:04 +0200

    Added setup files

---
+++ system_setup/install_google_dependencies.sh
@@ -0,0 +1,9 @@
+# Install gcloud
+sudo apt-get install curl
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update
+yes | sudo apt-get install google-cloud-sdk
+yes | sudo apt-get install google-cloud-sdk-app-engine-python
+yes | sudo apt-get install google-cloud-sdk-app-engine-python-extras
